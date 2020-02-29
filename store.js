bbced4c334cb09d32af3918689c65bc3c202b9ac
Micheal Limones <michlimones@gmail.com>
2020-02-29 12:12:17 -0700

    "added JS files and Images file. Updated HTML. Updated CSS to include responsive design."

---
+++ store.js
@@ -0,0 +1,106 @@
+const STORE = {
+    questions: [
+        {
+            question: "What is the name of the town where the story takes place?",
+            options: [
+                'Hulk Town',
+                'Riverdale',
+                'Hawkins',
+                'Springfield'
+            ],
+            answer: 'Hawkins'
+        },
+        {
+            question: "Who is the girl with telekinetic abilities?",
+            options: [
+                'Susie',
+                'Twelve', 
+                'Maxine',
+                'Eleven'
+            ],
+            answer: 'Eleven'
+        },
+        {
+            question: "Who is the boy that goes missing in the first season?",
+            options: [
+                'Will Byers',
+                'Jim Hopper',
+                'Bugs Bunny', 
+                'Mike Wheeler'
+            ],
+            answer: 'Will Byers'
+        },
+        {
+            question: "What decade does the story take place?",
+            options: [
+                '1990s',
+                '1980s',
+                '2010s',
+                '1940s'
+            ],
+            answer: '1980s'
+        },
+        {
+            question: "What do the kids call the place where Will was taken to in the first season?",
+            options: [
+                'The Upside Down',
+                'The Backwards Upwards',
+                'ToonTown',
+                'New York'
+            ],
+            answer: 'The Upside Down'
+        },
+        {
+            question: "Where does Steve work in season 3?",
+            options: [
+                "Scoops and more Scoops!", 
+                "Scoops Ahoy!",
+                "Taco Bell",
+                "Hawkin’s Post"
+            ],
+            answer: 'Scoops Ahoy!'
+        },
+        {
+            question: "What is the name of the creature the characters face in the first season?",
+            options: [
+                'The Mind Flayer',
+                'Bowser',
+                'The Demogorgon',
+                'Lord Voldemort'
+            ],
+            answer: 'THe Demogorgon'
+        },
+        {
+            question: "What is the name of the game the four boys would play in Mike’s basement?",
+            options: [
+                'Dungeons and Dragons',
+                'Mario Party',
+                'Connect Four',
+                'Twister'
+            ],
+            answer: 'Dungeons and Dragons'
+        },
+        {
+            question: "Who do the four boys dress up as for Halloween in the second season?",
+            options: [
+                'The Three Stooges',
+                'The Avengers',
+                'The Scooby Doo gang',
+                'The Ghostbusters'
+            ],
+            answer: 'The Ghostbusters'
+        },
+        {
+            question: "How does Joye Byers communicate with her missing son in the first season?",
+            options: [
+                'A Ouija board',
+                'Christmas lights',
+                'Postcards',
+                'Telegram'
+            ],
+            answer: 'Christmas lights'
+        }
+    ],
+    currentQuestion: 0,
+    score: 0,
+};
